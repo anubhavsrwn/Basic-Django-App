de6ccdb378927b35c6c7b916c80290f4d573ea69
Anubhav Sarwan <anubhav.s@technovert.net>
2021-07-20 14:03:40 +0530

    init

---
+++ home/views.py
@@ -0,0 +1,38 @@
+from typing import ContextManager
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        "variable": "this is it."
+    }
+    return render(request, "index.html", context)
+    # return HttpResponse("This is home page")
+
+
+def about(request):
+    return render(request, "about.html")
+    # return HttpResponse("This is about page")
+
+
+def services(request):
+    return render(request, "services.html")
+    # return HttpResponse("This is services page")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone,
+                          desc=desc, date=datetime.today())
+        contact.save()
+    return render(request, "contact.html")
+    # return HttpResponse("This is contact page")
